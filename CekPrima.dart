84ff8316ec8b3601d41d14033dc9373b4a25d9c2
alzaelf <nadinalfinazzahwa@gmail.com>
2026-03-30 05:21:44 +0700

    push program

--- CekPrima.dart
+++ CekPrima.dart
@@ -1,30 +1,29 @@
 import 'dart:io';
 
 bool CekPrima(int n){
-    if (n<2) return false;
-    if (n==2) return true;
-    if (n%2==0) return false;
+  if (n<2) return false;
+  if (n==2) return true;
+  if (n%2==0) return false;
 
-    for (int i=3; i*i <= n; i+=2){
-      if (n % i == 0) return false;
-    }
-    return true;
+  for ( int i=3; i*i <= n; i +=2){
+    if (n % i ==0) return false;
   }
-
-void main(){
-  print('Pengecekan Bilangan Prima');
-  print('Masukkan angka yang akan dicek: ');
+  return true;
+}
+void main() {
+  print ('Program Pengecekan Bilangan Prima');
+  print ('Masukkan angka yang akan dicek: ');
 
   String? input = stdin.readLineSync();
   int? number = int.tryParse(input ?? '');
 
   if (number != null){
-    if(CekPrima(number)) {
-      print('$number adalah bilangan prima');
+    if (CekPrima(number)) {
+      print ('$number adalah prima');
     } else {
-        print('$number bukan bilangan prima');
-      }
+      print('$number bukan bilangan prima');
+    }
   } else {
-    print('input tidak valid, harus berupa angka');
+    print('input tidak valid, masukkan angka');
   }
 }
